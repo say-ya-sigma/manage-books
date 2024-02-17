1472370072ae7d0a21892fd24ebe85e6ceca4265
sigma <seiya.sugimoto.free@gmail.com>
2024-02-18 05:49:53 +0900

    fix model file

--- app/orm/models/User.py
+++ app/orm/models/User.py
@@ -1,7 +1,7 @@
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, MappedAsDataclass, mapped_column
 
-from orm.models.settings import Base
+from orm.settings import Base
 
 
 class User(MappedAsDataclass, Base):
