69351e2985383351eff78c81a6b4a47c24a9eee4
sigma <seiya.sugimoto.free@gmail.com>
2024-02-20 03:41:53 +0900

    add get user action

--- app/presentation/route.py
+++ app/presentation/route.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask
 
 from presentation.api.user import actions, requests
 from presentation.decorators.adr import adr
@@ -6,6 +6,11 @@ from presentation.decorators.adr import adr
 router = Flask(__name__)
 
 @router.route("/")
-@adr(action=actions.hello_world.Action(), request=requests.hello_world.Request(request))
+@adr(ActionClass=actions.hello_world.Action, RequestClass=requests.hello_world.Request)
 def hello_world():
+    pass
+
+@router.route("/user/<int:id>")
+@adr(ActionClass=actions.get_user.Action, RequestClass=requests.get_user.Request)
+def get_user(id):
     pass
