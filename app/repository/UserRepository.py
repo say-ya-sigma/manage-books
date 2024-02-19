b35cae75c734de385038bc2b62b728f0ad230b8b
sigma <seiya.sugimoto.free@gmail.com>
2024-02-20 03:36:54 +0900

    add user repository

---
+++ app/repository/UserRepository.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+from entity.user.UserId import UserId
+from error.common import NotFoundException
+from orm.models.User import User
+
+
+class AbstractUserRepository(ABC):
+    @abstractmethod
+    def find_all(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def find_by_id(self, id: UserId):
+        raise NotImplementedError
+
+
+class UserRepository(AbstractUserRepository):
+    def find_all(self):
+        return map(lambda user: user.to_entity(), User.query.all())
+
+    def find_by_id(self, id: UserId):
+        user = User.query.filter(User.id == id.root).first()
+        if user is None:
+            raise NotFoundException()
+        return user.to_entity()
