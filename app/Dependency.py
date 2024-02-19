dec5f59bc37ed1b0f98391165fcb06fa252ac844
sigma <seiya.sugimoto.free@gmail.com>
2024-02-20 03:39:44 +0900

    add Dependency for di

---
+++ app/Dependency.py
@@ -0,0 +1,17 @@
+from injector import Binder, Injector
+
+from repository.UserRepository import AbstractUserRepository, UserRepository
+from service.UserService import UserService
+
+
+class Dependency:
+    def __init__(self) -> None:
+        self.injector = Injector(self.__class__.config)
+
+    @classmethod
+    def config(cls, binder: Binder):
+        binder.bind(AbstractUserRepository, to=UserRepository)
+        binder.bind(UserService, to=UserService)
+
+    def resolve(self, cls):
+        return self.injector.get(cls)
