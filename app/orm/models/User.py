377a76ee73838f04654adec4455e63406d04b80d
sigma <seiya.sugimoto.free@gmail.com>
2024-02-20 03:36:09 +0900

    add to_entity method to model

--- app/orm/models/User.py
+++ app/orm/models/User.py
@@ -1,6 +1,8 @@
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, MappedAsDataclass, mapped_column
 
+from entity.user.User import User as EntityUser
+from entity.user.UserId import UserId
 from orm.settings import Base
 
 
@@ -10,3 +12,11 @@ class User(MappedAsDataclass, Base):
     name: Mapped[str] = mapped_column(String(30))
     email: Mapped[str] = mapped_column(String(255))
     password: Mapped[str] = mapped_column(String(255))
+
+    def to_entity(self):
+        return EntityUser(
+            id=UserId(self.id),
+            name=self.name,
+            email=self.email,
+            password=self.password,
+        )
