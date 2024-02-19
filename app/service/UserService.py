d99574c517257a5c8cbe12f2d73e86a27c679928
sigma <seiya.sugimoto.free@gmail.com>
2024-02-20 03:39:26 +0900

    add service

---
+++ app/service/UserService.py
@@ -0,0 +1,13 @@
+from injector import inject
+
+from entity.user.UserId import UserId
+from repository.UserRepository import UserRepository
+
+
+class UserService:
+    @inject
+    def __init__(self, user_repository: UserRepository):
+        self.user_repository = user_repository
+
+    def get_user(self, user_id: UserId):
+        return self.user_repository.find_by_id(user_id)
