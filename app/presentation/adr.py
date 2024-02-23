113bc41b8a73f4dc6d86c7d2d2c4ca446043b42e
sigma <seiya.sugimoto.free@gmail.com>
2024-02-24 05:02:55 +0900

    fix adr decoration form has been abolished

---
+++ app/presentation/adr.py
@@ -0,0 +1,16 @@
+from Dependency import Dependency
+from flask import request as flask_request
+from presentation.interface.adr import AdrAction, AdrRequest
+from werkzeug.exceptions import UnprocessableEntity
+
+di = Dependency()
+
+
+def adr(ActionClass, RequestClass, **kwargs):
+    request: AdrRequest = RequestClass(flask_request, **kwargs)
+    valid = request.validate()
+    if not valid:
+        raise UnprocessableEntity("Invalid request")
+    action: AdrAction = di.resolve(ActionClass)
+    response = action.execute(request)
+    return response
