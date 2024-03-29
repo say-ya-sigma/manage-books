77ec66c657d7001184089b45556fe4f79b6813d0
sigma <seiya.sugimoto.free@gmail.com>
2024-03-30 02:01:11 +0900

    fix get id value

--- app/repository/UserRepository.py
+++ app/repository/UserRepository.py
@@ -20,7 +20,7 @@ class UserRepository(AbstractUserRepository):
         return map(lambda user: user.to_entity(), User.query.all())
 
     def find_by_id(self, id: UserId):
-        user = User.query.filter(User.id == id.root).first()
+        user = User.query.filter(User.id == id.value).first()
         if user is None:
             raise NotFoundException()
         return user.to_entity()
