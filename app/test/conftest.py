599578008c4a8e5e28ab5ee56b32ab1d820e78cd
sigma <seiya.sugimoto.free@gmail.com>
2024-05-04 15:00:44 +0900

    add conftest

---
+++ app/test/conftest.py
@@ -0,0 +1,54 @@
+import alembic.command
+import alembic.config
+import pytest
+from orm.seeders.run import run as seeder_run
+from orm.settings import Base
+from presentation.route import wsgi
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy_utils import create_database, database_exists, drop_database
+
+
+def migrate(
+    migrations_path,
+    db_url,
+    alembic_ini_path="alembic.ini",
+    connection=None,
+    revision="head"
+):
+    config = alembic.config.Config(alembic_ini_path)
+    config.set_main_option("sqlalchemy.url", db_url)
+    config.set_main_option("script_location", migrations_path)
+    if connection:
+        config.attributes["connection"] = connection
+    alembic.command.upgrade(config, revision)
+
+@pytest.fixture(scope="session")
+def client():
+    test_url = "postgresql://flask_user:flask_password@db:5432/flask_test_db"
+
+    # 既にテスト用データベースが存在していたら破棄する
+    if database_exists(test_url):
+        drop_database(test_url)
+
+    # テスト用データベースを作成する
+    create_database(test_url)
+
+    engine = create_engine(test_url)
+
+    with engine.begin() as connection:
+        # マイグレーションを実行
+        migrate("app/migrations",db_url=test_url, connection=connection)
+    engine.dispose()
+
+    session = scoped_session(sessionmaker(bind=engine))
+    Base.query = session.query_property()
+
+    wsgi.config["TESTING"] = True
+
+    with wsgi.test_client() as client:
+        seeder_run(session=session())
+
+        yield client
+
+        drop_database(test_url)
