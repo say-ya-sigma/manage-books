a8ba241ebbbbb2f18d6d8e1c5d624ef8cc5d2f6f
sigma <seiya.sugimoto.free@gmail.com>
2024-05-04 15:00:18 +0900

    add session to run seeders

--- app/orm/seeders/run.py
+++ app/orm/seeders/run.py
@@ -1,5 +1,10 @@
+from orm.seeders.seeders_session import seeders_session
+from sqlalchemy.orm import Session
+
 from .users_seeder import users_seeder
 
 
-def run():
-    users_seeder()
+def run(session: Session | None = None):
+    if session is None:
+        session = seeders_session
+    users_seeder(session)
