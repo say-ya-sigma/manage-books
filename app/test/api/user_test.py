6d847263bab41b219ad56da13329ae49bb0062d1
sigma <seiya.sugimoto.free@gmail.com>
2024-05-04 15:00:54 +0900

    add user_test for user api

---
+++ app/test/api/user_test.py
@@ -0,0 +1,22 @@
+def test_endpoint_response(client):
+    # エンドポイントにリクエストを送信
+    response = client.get("/user/1")
+
+    # レスポンスのステータスコードが200 (OK) であることを確認
+    assert response.status_code == 200
+
+    # レスポンスのJSONデータを取得
+    json_data = response.get_json()
+
+    # 期待するJSONデータ
+    expected_data = {
+        "id": {
+            "value": 1,
+        },
+        "name": "user1",
+        "email": "user1@vantan.jp",
+        "password": "password1"
+    }
+
+    # 実際のJSONデータと期待するJSONデータが一致することを確認
+    assert json_data == expected_data
