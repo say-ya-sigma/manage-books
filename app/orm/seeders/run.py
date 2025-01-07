950a13de541672ee6c1773996216cfb55a146a25
sigma <seiya.sugimoto.free@gmail.com>
2025-01-07 22:42:23 +0900

    add book category seeder to run command

--- app/orm/seeders/run.py
+++ app/orm/seeders/run.py
@@ -1,6 +1,7 @@
 from orm.seeders.seeders_session import seeders_session
 from sqlalchemy.orm import Session
 
+from .book_category_seeder import book_category_seeder
 from .users_seeder import users_seeder
 
 
@@ -8,4 +9,5 @@ def run(session: Session | None = None):
     if session is None:
         session = seeders_session
     users_seeder(session)
+    book_category_seeder(session)
     session.close()
