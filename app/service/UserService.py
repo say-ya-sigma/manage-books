e77194788f36617d77bad3164d887976f610d1d1
sigma <seiya.sugimoto.free@gmail.com>
2025-01-15 23:04:52 +0900

    fix user service

--- app/service/UserService.py
+++ app/service/UserService.py
@@ -1,6 +1,6 @@
 from entity.user import UserId
 from injector import inject
-from presentation.api.user.responders.get_user import GetUserResponseDto
+from presentation.api.user.responders import get_user
 from repository.UserRepository import AbstractUserRepository
 
 
@@ -9,6 +9,6 @@ class UserService:
     def __init__(self, user_repository: AbstractUserRepository):
         self.__user_repository = user_repository
 
-    def get_user(self, user_id: UserId) -> GetUserResponseDto:
+    def get_user(self, user_id: UserId) -> get_user.GetUserResponseDto:
         user = self.__user_repository.find_by_id(user_id)
-        return GetUserResponseDto(user)
+        return get_user.GetUserResponseDto(user)
