181c41d3f5f01e148dfd313ce0ff08a1909ebefb
sigma <seiya.sugimoto.free@gmail.com>
2025-01-25 20:26:53 +0900

    add book category seeder

--- app/orm/seeders/book_category_seeder.py
+++ app/orm/seeders/book_category_seeder.py
@@ -5,8 +5,8 @@ from sqlalchemy.orm import Session
 def book_category_seeder(session: Session):
     session.bulk_save_objects(
         [
-            BookCategory(1, "category1"),
-            BookCategory(2, "category2")
+            BookCategory("category1"),
+            BookCategory("category2")
         ]
     )
     session.commit()
