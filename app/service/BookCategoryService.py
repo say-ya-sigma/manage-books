880ba7c39c873e097990c75a05e3c4fe71e4ea46
sigma <seiya.sugimoto.free@gmail.com>
2025-01-25 20:29:37 +0900

    add create method to book category service

--- app/service/BookCategoryService.py
+++ app/service/BookCategoryService.py
@@ -1,6 +1,6 @@
 from entity.book.category import BookCategoryId
 from injector import inject
-from presentation.api.book.category.responders import get_category
+from presentation.api.book.category.responders import create_category, get_category
 from repository.BookCategoryRepository import AbstractBookCategoryRepository
 
 
@@ -15,3 +15,10 @@ class BookCategoryService:
     ) -> get_category.GetCategoryResponseDto:
         book_category = self.__book_category_repository.find_by_id(book_category_id)
         return get_category.GetCategoryResponseDto(book_category)
+
+    def create_book_category(
+        self,
+        name: str
+    ) -> create_category.CreateCategoryResponseDto:
+        book_category_id = self.__book_category_repository.create(name)
+        return create_category.CreateCategoryResponseDto(book_category_id)
