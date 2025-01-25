262ce585f6fbf50c0dbc1f5b8ecb14b1909d56f5
sigma <seiya.sugimoto.free@gmail.com>
2025-01-25 20:27:16 +0900

    add methods to repository

--- app/repository/BookCategoryRepository.py
+++ app/repository/BookCategoryRepository.py
@@ -10,6 +10,15 @@ class AbstractBookCategoryRepository(ABC):
     @abstractmethod
     def find_by_id(self, id: BookCategoryId) -> EntityBookCategory:
         raise NotImplementedError
+    @abstractmethod
+    def create(self, name: str) -> BookCategoryId:
+        raise NotImplementedError
+    @abstractmethod
+    def update(self, id: BookCategoryId, name: str) -> None:
+        raise NotImplementedError
+    @abstractmethod
+    def delete(self, id: BookCategoryId) -> None:
+        raise NotImplementedError
 
 
 class BookCategoryRepository(AbstractBookCategoryRepository):
@@ -18,3 +27,23 @@ class BookCategoryRepository(AbstractBookCategoryRepository):
         if book_category is None:
             raise NotFoundException()
         return book_category.to_entity()
+
+    def create(self, name):
+        book_category = BookCategory(name=name)
+        book_category.save()
+        return book_category.to_entity().id
+
+    def update(self, id, name):
+        book_category = BookCategory.query.filter(BookCategory.id == id.value).first()
+        if book_category is None:
+            raise NotFoundException()
+
+        book_category.name = name
+        book_category.save()
+
+    def delete(self, id):
+        book_category = BookCategory.query.filter(BookCategory.id == id.value).first()
+        if book_category is None:
+            raise NotFoundException()
+
+        book_category.delete()
