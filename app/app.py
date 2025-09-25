bae83b87b784cc99d34fad64ece3bce4e0fd2a13
sigma <seiya.sugimoto.free@gmail.com>
2025-09-25 13:09:12 +0900

    add session remove

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Response, json
 from presentation.route import wsgi
 from werkzeug.exceptions import HTTPException
+from orm.settings import session
 
 
 @wsgi.errorhandler(HTTPException)
@@ -15,6 +16,10 @@ def handle_exception(e: HTTPException):
         status=e.code,
         content_type="application/json"
     )
+
+@wsgi.teardown_request
+def remove_session(exception=None):
+    session.remove()
 
 if __name__ == "__main__":
     wsgi.run(host="0.0.0.0", port=8080, debug=True)
