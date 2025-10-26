1cba5ec539eddc1f235833f42c838ed8f26d35b8
sigma <seiya.sugimoto.free@gmail.com>
2025-10-26 21:03:49 +0900

    fix seeder user data

--- app/orm/seeders/users_seeder.py
+++ app/orm/seeders/users_seeder.py
@@ -1,3 +1,4 @@
+from bcrypt import hashpw
 from orm.models.User import User
 from sqlalchemy.orm import Session
 
@@ -5,9 +6,24 @@ from sqlalchemy.orm import Session
 def users_seeder(session: Session):
     session.bulk_save_objects(
         [
-            User(1, "user1", "user1@vantan.jp", "password1"),
-            User(2, "user2", "user2@vantan.jp", "password2"),
-            User(3, "user3", "user3@vantan.jp", "password3"),
+            User(
+                1,
+                "user1",
+                "user1@vantan.jp",
+                "$2b$12$Xb6gUKMsOYWGabGCq8pXY.tk9Afpzi.xA8RANxitbAW4ISI7c9VGS"
+            ), # bcrypt.hashpw(b"password1",bcrypt.gensalt()).decode()
+            User(
+                2,
+                "user2",
+                "user2@vantan.jp",
+                "$2b$12$20L2qseLQVZfbxhzwiRgQuaB4K069L4zdc.Crfebi8DVTVDwyWoe6"
+            ), # bcrypt.hashpw(b"password2",bcrypt.gensalt()).decode()
+            User(
+                3,
+                "user3",
+                "user3@vantan.jp",
+                "$2b$12$mAB5PmHFbkrzMmw59rX4uuMhCPJu25/Fhkg1pTYQgnlfdWE1E4ydG"
+            ), # bcrypt.hashpw(b"password2",bcrypt.gensalt()).decode()
         ]
     )
     session.commit()
